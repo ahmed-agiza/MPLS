96eb2235640f0bd4625ee3a6574e696336b6bb9e
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2014-11-28 12:03:22 +0200

    none

--- datamemory.cpp
+++ datamemory.cpp
@@ -1,35 +1,45 @@
 #include "datamemory.h"
 
-DataMemory::DataMemory(QObject *, Component *, size_t, int, bool, size_t){
+DataMemory::DataMemory(QObject * parent, Component * _sourceComponent, size_t , int _data, bool _writeEnabled, size_t _address)
+    :Component(parent, _sourceComponent){
+    this->_address = _address;
 
 }
 
 MEMWBBuffer *DataMemory::getBuffer() const{
-
+    return _buffer;
 }
 
-void DataMemory::setAddress(size_t){
-
+void DataMemory::setAddress(size_t _address){
+    this->_address = _address;
 }
 
 size_t DataMemory::getAddress() const{
-
+    return _address;
 }
 
-bool DataMemory::writeData(int){
-
+bool DataMemory::writeData(int _data){
+    if(_writeEnabled)
+    {
+        _values[_address] = _data;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 int DataMemory::getData() const{
 
 }
 
-void DataMemory::setWriteEnabled(bool){
-
+void DataMemory::setWriteEnabled(bool _writeEnabled){
+    this->_writeEnabled = _writeEnabled;
 }
 
 bool DataMemory::isWriteEnabled() const{
-
+    return _writeEnabled;
 }
 
 DataMemory::~DataMemory(){
