06456a77d26976d29bba155cca34eb26c14b2afb
IbrahimSba3i <el-sebaie.ebrahim@aucegypt.edu>
2014-11-28 23:00:06 +0200

    started the parsing

--- simulator.cpp
+++ simulator.cpp
@@ -9,6 +9,15 @@ Simulator::Simulator(QObject *parent, QStringList rawInstructions)
 
 bool Simulator::parseInstructions(){
 
+    QRegExp registerRegex(QString("\\$((?:[12]?[\\d])|(?:3[012])|(?:zero)|(?:at)|(?:v[01])|(?:a[0-3])|(?:t\\d)|(?:s[0-7])|(?:k[01])|gp|fp|ra|sp)"), Qt::CaseInsensitive);
+    QRegExp commentRegex(QString("#.*"));
+    QRegExp labelRegex(QString("[a-zA-Z_]\\w*"));
+    QRegExp numberRegex(QString("(0x[0-9a-fA-F]+|-?\\d+|0b[01]+)"));
+
+    foreach(const QString& line, _rawInstructions)
+    {
+        _instructions.append(new Instruction(this,InstructionName::ADD));
+    }
     return false;
 }
 
