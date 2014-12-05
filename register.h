63628187a79e399d541e430050d19f567e593931
ahmed-agiza <ahmed.agiza22@gmail.com>
2014-12-06 00:20:24 +0200

    Follow Up

--- register.h
+++ register.h
@@ -6,6 +6,12 @@
 enum class RegisterName {UNDEF, ZERO, AT, V0, V1, A0, A1, A2, A3, T0, T1, T2, T3, T4, T5, T6, T7, S0, S1, S2, S3, S4, S5, S6, S7, T8, T9, K0, K1, GP, SP, FP, RA, PC, BUFF, CUSTOM};
 enum class RegisterState{STABLE, WRITING};
 
+
+typedef std::pair<unsigned int, RegisterName> IntNameP;
+typedef std::pair< RegisterName, QString > NameStringP;
+typedef std::pair<QString, int> StringIntP;
+
+
 class Register : public Component
 {
     Q_OBJECT
@@ -15,10 +21,12 @@ protected:
     RegisterName _name;
     RegisterState _state;
 
-    static QMap< unsigned int, RegisterName > _regNums;
-    static QMap< RegisterName, QString > _regNames;
-    static QMap< QString, int > _regNumbers;
-    static void _initializeRegistersMap();
+    static const QMap< unsigned int, RegisterName > _regNums;
+    static const QMap< RegisterName, QString > _regNames;
+    static const QMap< QString, int > _regNumbers;
+    //static void _initializeRegistersMap();
+
+    friend class Core;
 public:
     Register(QObject * = 0, Component * = 0, int = 0, RegisterName = RegisterName::UNDEF);
 
